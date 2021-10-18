55008102ec625d92fce54da045ddb72f293a40ce
leena <annachandy123@gmail.com>
2021-10-18 22:54:26 +1100

    renderlicense functions added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,20 +4,16 @@ function renderLicenseBadge(license) {
     let licenseID;
     switch (license) {
         case "MIT":
-            licenseID =
-                "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+            licenseID = "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
             break;
         case "Apache 2.0":
-            licenseID =
-                "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+            licenseID = "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
             break;
         case "GPL 3.0":
-            licenseID =
-                "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+            licenseID = "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
             break;
         case "Mozilla 2.0":
-            licenseID =
-                "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+            licenseID = "![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)";
             break;
         case "None":
             licenseID = "";
@@ -58,13 +54,13 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    const licenseLink = renderLicenseLink(license);
+    return licenseLink;
+    // console.log(licenseLink, "license link");
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
-   
-`;
-}
+function generateMarkdown(data) {}
 
 module.exports = generateMarkdown;
